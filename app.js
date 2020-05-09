aa5d85e9926d137defa1acc0aba8ce55604e4ae3
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-09 10:52:17 +0200

    Started listening for requests.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Listening for requests on port ${port}.`);
+});

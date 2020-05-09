a395eb85ef5c6b40c05544e89fd27475e2d51236
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-09 12:04:56 +0200

    Created routes for all users, and user with specific id.

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const app = express();
 
-const port = 8080;
-// http://mywesbite.com/homepage
-// http://mywesbite.com/users
-// http://mywesbite.com/
+function User(id, username, email, dateOfBirth, country) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.dateOfBirth = dateOfBirth;
+    this.country = country;
+};
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>');
-});
+const users = [
+    new User(1, 'username1', 'email1@gmail.com', '1990-05-05', 'Kosova'),
+    new User(2, 'username2', 'email2@gmail.com', '1992-06-06', 'USA'),
+    new User(3, 'username3', 'email3@gmail.com', '1993-07-07', 'Germany'),
+    new User(4, 'username4', 'email4@gmail.com', '1999-08-08', 'Spain')
+];
 
-app.get('/homepage', (req, res) => {
-    res.send('You are in homepage.');
-});
+// http://mywesbsite.com/users => Retrun all users
+// http://mywesbsite.com/users/{id} => Retrun user with id {id}
 
 app.get('/users', (req, res) => {
-    res.send('You are in users page.');
+    res.json(users);
 });
 
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    let user;
+    for (let i = 0; i < users.length; i++) {
+        console.log(users[i].id, id)
+        if (users[i].id == id) {
+            user = users[i];
+        }
+    }
+    if (!user) {
+        res.json({ error: 'A user with the given ID was not found.'});
+    } else {
+        res.json(user);
+    }
+});
+
+const port = 8080;
+
 app.listen(port, () => {
-    console.log(`Listenin for on port ${port}.`);
+    console.log(`Listening for on port ${port}.`);
 });

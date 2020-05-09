fd13c9e4b61446f931138389e22eac96b6a194a4
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-09 11:22:35 +0200

    Created three routes.

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 
 const port = 8080;
+// http://mywesbite.com/homepage
+// http://mywesbite.com/users
+// http://mywesbite.com/
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello</h1>');
+});
+
+app.get('/homepage', (req, res) => {
+    res.send('You are in homepage.');
+});
+
+app.get('/users', (req, res) => {
+    res.send('You are in users page.');
+});
 
 app.listen(port, () => {
     console.log(`Listenin for on port ${port}.`);

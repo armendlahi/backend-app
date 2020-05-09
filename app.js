4d56911d237fed57eb186c28abe47c6da772680b
Entor Arifi <entor.arifi@jaywalker-digital.ch>
2020-05-09 12:12:56 +0200

    Added create route for users.

--- app.js
+++ app.js
@@ -23,20 +23,21 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    let user;
-    for (let i = 0; i < users.length; i++) {
-        console.log(users[i].id, id)
-        if (users[i].id == id) {
-            user = users[i];
-        }
-    }
+app.get('/users/find/:id', (req, res) => {
+    const user = users.find(user => user.id == req.params.id);
+
     if (!user) {
         res.json({ error: 'A user with the given ID was not found.'});
     } else {
         res.json(user);
     }
+});
+
+app.get('/users/create', (req, res) => {
+    const user = new User(5, 'username5', 'email5@gmail.com', '1990-08-08', 'Kosova');
+    users.push(user);
+
+    res.json(user);
 });
 
 const port = 8080;
